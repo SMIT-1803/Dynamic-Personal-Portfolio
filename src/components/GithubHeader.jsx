63ecd9aecbc7b9b71b72d0c0c25e2c481aa840c9
SMIT-1803 <smitsachin1@gmail.com>
2025-06-05 00:30:46 -0700

    Header bug

--- src/components/GithubHeader.jsx
+++ src/components/GithubHeader.jsx
@@ -4,7 +4,7 @@ function GithubHeader() {
   return (
     <>
       <div
-        className="relative bg-black text-white flex w-full justify-center items-center 
+        className="relative bg-black text-white flex w-screen justify-center items-center 
       p-4 text-sm cursor-pointer hover:text-orange-400 transition-all md:p-3"
       >
         <a href="https://github.com/SMIT-1803" className="gC" target="blank">
